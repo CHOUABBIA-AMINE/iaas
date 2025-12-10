1ab66395d086a112585ea280f4bf01db0eea72bd
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-10 15:23:38 +0100

    Fix ContractDTO: parameterize GenericDTO<Contract> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/contract/dto/ContractDTO.java
+++ src/main/java/dz/mdn/iaas/business/contract/dto/ContractDTO.java
@@ -17,12 +17,14 @@ package dz.mdn.iaas.business.contract.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.contract.model.Contract;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Contract Data Transfer Object
@@ -30,10 +32,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ContractDTO extends GenericDTO {
+public class ContractDTO extends GenericDTO<Contract> {
 
     @NotBlank(message = "Internal ID is required")
     private String internalId;
@@ -77,4 +80,74 @@ public class ContractDTO extends GenericDTO {
     
     private Long contractUpId;
 
+    @Override
+    public Contract toEntity() {
+        Contract entity = new Contract();
+        entity.setId(this.getId());
+        entity.setInternalId(this.internalId);
+        entity.setContractYear(this.contractYear);
+        entity.setReference(this.reference);
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        entity.setAmount(this.amount);
+        entity.setTransferableAmount(this.transferableAmount);
+        entity.setStartDate(this.startDate);
+        entity.setApprovalReference(this.approvalReference);
+        entity.setApprovalDate(this.approvalDate);
+        entity.setContractDate(this.contractDate);
+        entity.setNotifyDate(this.notifyDate);
+        entity.setContractDuration(this.contractDuration);
+        entity.setObservation(this.observation);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(Contract entity) {
+        if (this.internalId != null) {
+            entity.setInternalId(this.internalId);
+        }
+        if (this.contractYear != null) {
+            entity.setContractYear(this.contractYear);
+        }
+        if (this.reference != null) {
+            entity.setReference(this.reference);
+        }
+        if (this.designationAr != null) {
+            entity.setDesignationAr(this.designationAr);
+        }
+        if (this.designationEn != null) {
+            entity.setDesignationEn(this.designationEn);
+        }
+        if (this.designationFr != null) {
+            entity.setDesignationFr(this.designationFr);
+        }
+        if (this.amount > 0) {
+            entity.setAmount(this.amount);
+        }
+        if (this.transferableAmount >= 0) {
+            entity.setTransferableAmount(this.transferableAmount);
+        }
+        if (this.startDate != null) {
+            entity.setStartDate(this.startDate);
+        }
+        if (this.approvalReference != null) {
+            entity.setApprovalReference(this.approvalReference);
+        }
+        if (this.approvalDate != null) {
+            entity.setApprovalDate(this.approvalDate);
+        }
+        if (this.contractDate != null) {
+            entity.setContractDate(this.contractDate);
+        }
+        if (this.notifyDate != null) {
+            entity.setNotifyDate(this.notifyDate);
+        }
+        if (this.contractDuration > 0) {
+            entity.setContractDuration(this.contractDuration);
+        }
+        if (this.observation != null) {
+            entity.setObservation(this.observation);
+        }
+    }
 }
