6c5e1ea1ccfb9626984e5050ecbeee75fe876445
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 12:51:59 +0100

    Update Common/Administration models (part 3 - FINAL): Structure, StructureType - extend GenericModel - ALL ADMINISTRATION MODELS COMPLETE!

--- src/main/java/dz/mdn/iaas/common/administration/model/Structure.java
+++ src/main/java/dz/mdn/iaas/common/administration/model/Structure.java
@@ -1,74 +1,62 @@
 /**
  *	
  *	@author		: CHOUABBIA Amine
- *
  *	@Name		: Structure
  *	@CreatedOn	: 06-26-2025
- *
- *	@Type		: Class
- *	@Layaer		: Model
- *	@Package	: Common / Administration
+ *	@Updated	: 12-11-2025
+ *	@Type		: Model
+ *	@Layer		: Common / Administration
+ *	@Package	: Common / Administration / Model
  *
  **/
 
 package dz.mdn.iaas.common.administration.model;
 
+import dz.mdn.iaas.configuration.template.GenericModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+/**
+ * Structure Entity - Extends GenericModel
+ * Represents organizational structures with multilingual designations and hierarchy
+ */
 @Setter
 @Getter
-@ToString
-@EqualsAndHashCode
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="Structure")
-@Table(name="T_01_04_07", uniqueConstraints = { @UniqueConstraint(name = "T_01_04_07_UK_01", columnNames = { "F_03" }), 
-											 	@UniqueConstraint(name = "T_01_04_07_UK_02", columnNames = { "F_06" })})
-public class Structure {
+@Table(name="T_01_04_09", uniqueConstraints = { 
+    @UniqueConstraint(name = "T_01_04_09_UK_01", columnNames = { "F_06" })
+})
+public class Structure extends GenericModel {
 	
-	@Id
-	@Column(name="F_00")
-  	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name="F_01", length=200)
+	@Column(name="F_01", length=100)
 	private String designationAr;
-	
-	@Column(name="F_02", length=200)
+
+	@Column(name="F_02", length=100)
 	private String designationEn;
 	
-	@Column(name="F_03", length=200, nullable=false)
+	@Column(name="F_03", length=100, nullable=false)
 	private String designationFr;
 	
-	@Column(name="F_04", length=20)
-	private String acronymAr;
-	
-	@Column(name="F_05", length=20)
-	private String acronymEn;
-	
-	@Column(name="F_06", length=20, nullable=false)
-	private String acronymFr;
+	@ManyToOne
+	@JoinColumn(name="F_04")
+	private Structure parentStructure;
 	
 	@ManyToOne
-    @JoinColumn(name="F_07", foreignKey=@ForeignKey(name="T_01_04_07_FK_01"), nullable=false)
-    private StructureType structureType;
+	@JoinColumn(name="F_05", nullable=false)
+	private StructureType structureType;
 	
-	@ManyToOne
-    @JoinColumn(name="F_08", foreignKey=@ForeignKey(name="T_01_04_07_FK_02"), nullable=true)
-    private Structure structureUp;
+	@Column(name="F_06", length=50, nullable=false)
+	private String code;
 }
--- src/main/java/dz/mdn/iaas/common/administration/model/StructureType.java
+++ src/main/java/dz/mdn/iaas/common/administration/model/StructureType.java
@@ -1,97 +1,69 @@
 /**
  *	
  *	@author		: CHOUABBIA Amine
- *
  *	@Name		: StructureType
  *	@CreatedOn	: 06-26-2025
- *
- *	@Type		: Class
- *	@Layaer		: Model
- *	@Package	: Common / Administration
+ *	@Updated	: 12-11-2025
+ *	@Type		: Model
+ *	@Layer		: Common / Administration
+ *	@Package	: Common / Administration / Model
  *
  **/
 
 package dz.mdn.iaas.common.administration.model;
 
+import dz.mdn.iaas.configuration.template.GenericModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+/**
+ * StructureType Entity - Extends GenericModel
+ * Represents types of organizational structures with multilingual designations
+ */
 @Setter
 @Getter
-@ToString
-@EqualsAndHashCode
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="StructureType")
-@Table(name="T_01_04_06", uniqueConstraints = { @UniqueConstraint(name = "T_01_04_06_UK_01", columnNames = { "F_03" })})
-public class StructureType {
+@Table(name="T_01_04_10", uniqueConstraints = { 
+    @UniqueConstraint(name = "T_01_04_10_UK_01", columnNames = { "F_04" }),
+    @UniqueConstraint(name = "T_01_04_10_UK_02", columnNames = { "F_05" }),
+    @UniqueConstraint(name = "T_01_04_10_UK_03", columnNames = { "F_06" })
+})
+public class StructureType extends GenericModel {
 	
-	@Id
-	@Column(name="F_00")
-  	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name="F_01", length=200)
+	@Column(name="F_01", length=100)
 	private String designationAr;
-	
-	@Column(name="F_02", length=200)
+
+	@Column(name="F_02", length=100)
 	private String designationEn;
 	
-	@Column(name="F_03", length=200, nullable=false)
+	@Column(name="F_03", length=100, nullable=false)
 	private String designationFr;
 	
+	@Column(name="F_04", length=100)
+	private String acronymAr;
+
+	@Column(name="F_05", length=100)
+	private String acronymEn;
+	
+	@Column(name="F_06", length=100, nullable=false)
+	private String acronymFr;
+	
+	@Column(name="F_07")
+	private Integer hierarchyLevel;
+	
+	@Column(name="F_08")
+	private Boolean isOperational;
+	
+	@Column(name="F_09")
+	private Boolean canHaveSubStructures;
 }
-/*
-Insert into T_01_04_06 (F_00, F_01, F_02, F_03) values
-(1,'وزارة','Ministry','Ministère'),
-(2,' أركان الجيش','Army General Staff','Etat Major de l\'Armée'),
-(3,'مديرية عامة','General Directorate','Direction Générale'),
-(4,'الأمانة العامة لوزارة','Ministry General Secretariat','Secretariat Générale du Ministère'),
-(5,'ديوان','Cabinet','Cabinet'),
-(6,'قيادة قوات','Force Command','Commandement de Forces'),
-(7,'ناحية عسكرية','Military Region','Région Militaire'),
-(8,' أركان ناحية عسكرية','Military Region Staff','Etat Major d\'une Région Militaire'),
-(9,'دائرة','Department','Département'),
-(10,'مديرية مركزية','Central Directorate','Direction Centrale'),
-(11,'مديرية','Directorate','Direction'),
-(12,'مصلحة مركزية','Central Service','Service Central'),
-(13,'مصلحة','Service','Service'),
-(14,'مركز','Center','Centre'),
-(15,'مديرية دائرة','Department Directorate','Direction de Département'),
-(16,'مصلحة دائرة','Department Service','Service de Département'),
-(17,'مكتب دائرة','Department Office','Bureau de Département'),
-(18,'فصيلة','Section','Section'),
-(19,'مديرية جهوية','Regional Directorate','Direction Régionale'),
-(20,'مصلحة جهوية','Regional Service','Service Régional'),
-(21,'مكتب جهوي','Regional Office','Bureau Régional'),
-(22,'مركز جهوي','Regional Center','Centre Régional'),
-(23,'مديرية فرعية','Sub-Directorate','Sous Direction'),
-(24,'خلية','Cell','Cellule'),
-(25,'فصيلة فرعية','Subsection','Sous Section'),
-(26,'مكتب','Office','Bureau'),
-(27,'فصيلة مكتب','Office Section','Section de Bureau'),
-(28,'مدرسة وطنية','National School','Ecole Nationale'),
-(29,'مدرسة عليا','Higher School','Ecole Supérieure'),
-(30,'مدرسة تطبيقية','Application School','Ecole d\'Application'),
-(31,'مركز تدريب','Training Center','Centre d\'instruction'),
-(32,'مؤسسة وزارية','Ministerial Establishment','Etablissement Ministèriel'),
-(33,'مؤسسة مركزية','Central Establishment','Etablissement Centrale'),
-(34,'مؤسسة جهوية','Regional Establishment','Etablissement Régional'),
-(35,'فرقة','Division','Division'),
-(36,'لواء','Brigade','Brigade'),
-(37,'فوج','Regiment','Régiment'),
-(38,'فوج جهوي','Regional Regiment','Régiment Régional'),
-(39,'كتيبة','Battalion','Bataillon'),
-(40,'سرية','Company','Compagnie');
-*/
