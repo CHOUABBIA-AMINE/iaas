c635cb7589b34b39452e1ac1f10c8eb327adaaa1
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:27:04 +0100

    Fix RubricDTO: parameterize GenericDTO<Rubric> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/plan/dto/RubricDTO.java
+++ src/main/java/dz/mdn/iaas/business/plan/dto/RubricDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: RubricDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -16,12 +16,14 @@ package dz.mdn.iaas.business.plan.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.plan.model.Rubric;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Rubric Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class RubricDTO extends GenericDTO {
+public class RubricDTO extends GenericDTO<Rubric> {
 
     @NotBlank(message = "Code is required")
     private String code;
@@ -50,4 +53,24 @@ public class RubricDTO extends GenericDTO {
     private String description;
     private Long parentRubricId;
 
+    @Override
+    public Rubric toEntity() {
+        Rubric entity = new Rubric();
+        entity.setId(this.getId());
+        entity.setCode(this.code);
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        entity.setDescription(this.description);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(Rubric entity) {
+        if (this.code != null) entity.setCode(this.code);
+        if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
+        if (this.designationEn != null) entity.setDesignationEn(this.designationEn);
+        if (this.designationFr != null) entity.setDesignationFr(this.designationFr);
+        if (this.description != null) entity.setDescription(this.description);
+    }
 }
