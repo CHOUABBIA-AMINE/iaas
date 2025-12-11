3212ed3e709640d2c5986d3ee72602df78050ae9
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:24:21 +0100

    Fix BudgetModificationDTO: parameterize GenericDTO<BudgetModification> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/plan/dto/BudgetModificationDTO.java
+++ src/main/java/dz/mdn/iaas/business/plan/dto/BudgetModificationDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: BudgetModificationDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,11 +17,13 @@ package dz.mdn.iaas.business.plan.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.plan.model.BudgetModification;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * BudgetModification Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BudgetModificationDTO extends GenericDTO {
+public class BudgetModificationDTO extends GenericDTO<BudgetModification> {
 
     private int internalId;
     private String reference;
@@ -51,4 +54,15 @@ public class BudgetModificationDTO extends GenericDTO {
     
     private Long approvalStatusId;
 
+    @Override
+    public BudgetModification toEntity() {
+        BudgetModification entity = new BudgetModification();
+        entity.setId(this.getId());
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(BudgetModification entity) {
+        // Update logic for fields present in model
+    }
 }
