cff5f2c0056ce637e763252b0c3a00adb617ff96
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 11:29:39 +0100

    Fix EconomicNatureDTO: align with model (code + 3 designations + 3 acronyms, remove economicDomainId)

--- src/main/java/dz/mdn/iaas/business/provider/dto/EconomicNatureDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/EconomicNatureDTO.java
@@ -19,7 +19,6 @@ import dz.mdn.iaas.configuration.template.GenericDTO;
 import dz.mdn.iaas.business.provider.model.EconomicNature;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -38,21 +37,28 @@ import lombok.experimental.SuperBuilder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class EconomicNatureDTO extends GenericDTO<EconomicNature> {
 
-    @NotBlank(message = "Code is required")
+    @Size(max = 20, message = "Code must not exceed 20 characters")
     private String code;
     
-    @Size(max = 300, message = "Arabic designation must not exceed 300 characters")
+    @Size(max = 200, message = "Arabic designation must not exceed 200 characters")
     private String designationAr;
 
-    @Size(max = 300, message = "English designation must not exceed 300 characters")
+    @Size(max = 200, message = "English designation must not exceed 200 characters")
     private String designationEn;
 
     @NotBlank(message = "French designation is required")
-    @Size(max = 300, message = "French designation must not exceed 300 characters")
+    @Size(max = 200, message = "French designation must not exceed 200 characters")
     private String designationFr;
     
-    @NotNull(message = "Economic domain is required")
-    private Long economicDomainId;
+    @Size(max = 20, message = "Arabic acronym must not exceed 20 characters")
+    private String acronymAr;
+    
+    @Size(max = 20, message = "English acronym must not exceed 20 characters")
+    private String acronymEn;
+    
+    @NotBlank(message = "French acronym is required")
+    @Size(max = 20, message = "French acronym must not exceed 20 characters")
+    private String acronymFr;
 
     @Override
     public EconomicNature toEntity() {
@@ -62,6 +68,9 @@ public class EconomicNatureDTO extends GenericDTO<EconomicNature> {
         entity.setDesignationAr(this.designationAr);
         entity.setDesignationEn(this.designationEn);
         entity.setDesignationFr(this.designationFr);
+        entity.setAcronymAr(this.acronymAr);
+        entity.setAcronymEn(this.acronymEn);
+        entity.setAcronymFr(this.acronymFr);
         return entity;
     }
 
@@ -71,6 +80,9 @@ public class EconomicNatureDTO extends GenericDTO<EconomicNature> {
         if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
         if (this.designationEn != null) entity.setDesignationEn(this.designationEn);
         if (this.designationFr != null) entity.setDesignationFr(this.designationFr);
+        if (this.acronymAr != null) entity.setAcronymAr(this.acronymAr);
+        if (this.acronymEn != null) entity.setAcronymEn(this.acronymEn);
+        if (this.acronymFr != null) entity.setAcronymFr(this.acronymFr);
     }
 
     public static EconomicNatureDTO fromEntity(EconomicNature entity) {
@@ -81,7 +93,9 @@ public class EconomicNatureDTO extends GenericDTO<EconomicNature> {
                 .designationAr(entity.getDesignationAr())
                 .designationEn(entity.getDesignationEn())
                 .designationFr(entity.getDesignationFr())
-                .economicDomainId(entity.getEconomicDomain() != null ? entity.getEconomicDomain().getId() : null)
+                .acronymAr(entity.getAcronymAr())
+                .acronymEn(entity.getAcronymEn())
+                .acronymFr(entity.getAcronymFr())
                 .build();
     }
 }
