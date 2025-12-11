f5a4d2055c64d09b39e5bcf22fb74ce777ae3640
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:28:15 +0100

    Fix ProviderDTO: parameterize GenericDTO<Provider> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/provider/dto/ProviderDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/ProviderDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: ProviderDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,6 +17,7 @@ package dz.mdn.iaas.business.provider.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.provider.model.Provider;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -24,6 +25,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Provider Data Transfer Object
@@ -31,10 +33,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ProviderDTO extends GenericDTO {
+public class ProviderDTO extends GenericDTO<Provider> {
 
     private int internalId;
     
@@ -66,4 +69,34 @@ public class ProviderDTO extends GenericDTO {
     @NotNull(message = "Economic nature is required")
     private Long economicNatureId;
 
+    @Override
+    public Provider toEntity() {
+        Provider entity = new Provider();
+        entity.setId(this.getId());
+        entity.setDesignationLt(this.name);
+        entity.setDesignationAr(this.nameAr);
+        entity.setAddress(this.address);
+        entity.setTaxeIdentityNumber(this.taxIdentificationNumber);
+        entity.setComercialRegistryNumber(this.commercialRegisterNumber);
+        entity.setPhoneNumbers(this.phone);
+        entity.setFaxNumbers(this.fax);
+        entity.setMail(this.email);
+        entity.setWebsite(this.website);
+        entity.setComercialRegistryDate(this.registrationDate);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(Provider entity) {
+        if (this.name != null) entity.setDesignationLt(this.name);
+        if (this.nameAr != null) entity.setDesignationAr(this.nameAr);
+        if (this.address != null) entity.setAddress(this.address);
+        if (this.taxIdentificationNumber != null) entity.setTaxeIdentityNumber(this.taxIdentificationNumber);
+        if (this.commercialRegisterNumber != null) entity.setComercialRegistryNumber(this.commercialRegisterNumber);
+        if (this.phone != null) entity.setPhoneNumbers(this.phone);
+        if (this.fax != null) entity.setFaxNumbers(this.fax);
+        if (this.email != null) entity.setMail(this.email);
+        if (this.website != null) entity.setWebsite(this.website);
+        if (this.registrationDate != null) entity.setComercialRegistryDate(this.registrationDate);
+    }
 }
