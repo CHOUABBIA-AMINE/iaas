2e6e699227aba6157298fe94a508ae2d44f259f3
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:36:27 +0100

    Fix ProviderDTO: align all fields with Provider model (18 fields + 4 relations)

--- src/main/java/dz/mdn/iaas/business/provider/dto/ProviderDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/ProviderDTO.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
 import dz.mdn.iaas.business.provider.model.Provider;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -39,64 +38,123 @@ import lombok.experimental.SuperBuilder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProviderDTO extends GenericDTO<Provider> {
 
-    private int internalId;
+    @NotBlank(message = "Latin designation is required")
+    private String designationLt;
     
-    @NotBlank(message = "Provider name is required")
-    private String name;
+    private String designationAr;
     
-    private String nameAr;
-    private String nameEn;
+    private String acronymLt;
     
-    @NotBlank(message = "Tax identification number is required")
-    private String taxIdentificationNumber;
+    private String acronymAr;
     
-    private String commercialRegisterNumber;
     private String address;
-    private String city;
-    private String postalCode;
-    private String country;
     
-    @Email(message = "Invalid email format")
-    private String email;
+    private double capital;
     
-    private String phone;
-    private String fax;
+    private String comercialRegistryNumber;
+    
+    private Date comercialRegistryDate;
+    
+    @NotBlank(message = "Tax identity number is required")
+    private String taxeIdentityNumber;
+    
+    private String statIdentityNumber;
+    
+    private String bank;
+    
+    private String bankAccount;
+    
+    private String swiftNumber;
+    
+    private String phoneNumbers;
+    
+    private String faxNumbers;
+    
+    private String mail;
+    
     private String website;
-    private Date registrationDate;
-    private boolean isActive;
-    private String observation;
+    
+    private Long logoId;
     
     @NotNull(message = "Economic nature is required")
     private Long economicNatureId;
+    
+    @NotNull(message = "Country is required")
+    private Long countryId;
+    
+    private Long stateId;
 
     @Override
     public Provider toEntity() {
         Provider entity = new Provider();
         entity.setId(this.getId());
-        entity.setDesignationLt(this.name);
-        entity.setDesignationAr(this.nameAr);
+        entity.setDesignationLt(this.designationLt);
+        entity.setDesignationAr(this.designationAr);
+        entity.setAcronymLt(this.acronymLt);
+        entity.setAcronymAr(this.acronymAr);
         entity.setAddress(this.address);
-        entity.setTaxeIdentityNumber(this.taxIdentificationNumber);
-        entity.setComercialRegistryNumber(this.commercialRegisterNumber);
-        entity.setPhoneNumbers(this.phone);
-        entity.setFaxNumbers(this.fax);
-        entity.setMail(this.email);
+        entity.setCapital(this.capital);
+        entity.setComercialRegistryNumber(this.comercialRegistryNumber);
+        entity.setComercialRegistryDate(this.comercialRegistryDate);
+        entity.setTaxeIdentityNumber(this.taxeIdentityNumber);
+        entity.setStatIdentityNumber(this.statIdentityNumber);
+        entity.setBank(this.bank);
+        entity.setBankAccount(this.bankAccount);
+        entity.setSwiftNumber(this.swiftNumber);
+        entity.setPhoneNumbers(this.phoneNumbers);
+        entity.setFaxNumbers(this.faxNumbers);
+        entity.setMail(this.mail);
         entity.setWebsite(this.website);
-        entity.setComercialRegistryDate(this.registrationDate);
         return entity;
     }
 
     @Override
     public void updateEntity(Provider entity) {
-        if (this.name != null) entity.setDesignationLt(this.name);
-        if (this.nameAr != null) entity.setDesignationAr(this.nameAr);
+        if (this.designationLt != null) entity.setDesignationLt(this.designationLt);
+        if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
+        if (this.acronymLt != null) entity.setAcronymLt(this.acronymLt);
+        if (this.acronymAr != null) entity.setAcronymAr(this.acronymAr);
         if (this.address != null) entity.setAddress(this.address);
-        if (this.taxIdentificationNumber != null) entity.setTaxeIdentityNumber(this.taxIdentificationNumber);
-        if (this.commercialRegisterNumber != null) entity.setComercialRegistryNumber(this.commercialRegisterNumber);
-        if (this.phone != null) entity.setPhoneNumbers(this.phone);
-        if (this.fax != null) entity.setFaxNumbers(this.fax);
-        if (this.email != null) entity.setMail(this.email);
+        entity.setCapital(this.capital);
+        if (this.comercialRegistryNumber != null) entity.setComercialRegistryNumber(this.comercialRegistryNumber);
+        if (this.comercialRegistryDate != null) entity.setComercialRegistryDate(this.comercialRegistryDate);
+        if (this.taxeIdentityNumber != null) entity.setTaxeIdentityNumber(this.taxeIdentityNumber);
+        if (this.statIdentityNumber != null) entity.setStatIdentityNumber(this.statIdentityNumber);
+        if (this.bank != null) entity.setBank(this.bank);
+        if (this.bankAccount != null) entity.setBankAccount(this.bankAccount);
+        if (this.swiftNumber != null) entity.setSwiftNumber(this.swiftNumber);
+        if (this.phoneNumbers != null) entity.setPhoneNumbers(this.phoneNumbers);
+        if (this.faxNumbers != null) entity.setFaxNumbers(this.faxNumbers);
+        if (this.mail != null) entity.setMail(this.mail);
         if (this.website != null) entity.setWebsite(this.website);
-        if (this.registrationDate != null) entity.setComercialRegistryDate(this.registrationDate);
+    }
+
+    public static ProviderDTO fromEntity(Provider entity) {
+        if (entity == null) return null;
+        
+        return ProviderDTO.builder()
+                .id(entity.getId())
+                .designationLt(entity.getDesignationLt())
+                .designationAr(entity.getDesignationAr())
+                .acronymLt(entity.getAcronymLt())
+                .acronymAr(entity.getAcronymAr())
+                .address(entity.getAddress())
+                .capital(entity.getCapital())
+                .comercialRegistryNumber(entity.getComercialRegistryNumber())
+                .comercialRegistryDate(entity.getComercialRegistryDate())
+                .taxeIdentityNumber(entity.getTaxeIdentityNumber())
+                .statIdentityNumber(entity.getStatIdentityNumber())
+                .bank(entity.getBank())
+                .bankAccount(entity.getBankAccount())
+                .swiftNumber(entity.getSwiftNumber())
+                .phoneNumbers(entity.getPhoneNumbers())
+                .faxNumbers(entity.getFaxNumbers())
+                .mail(entity.getMail())
+                .website(entity.getWebsite())
+                .logoId(entity.getLogo() != null ? entity.getLogo().getId() : null)
+                .economicNatureId(entity.getEconomicNature() != null ? entity.getEconomicNature().getId() : null)
+                .countryId(entity.getCountry() != null ? entity.getCountry().getId() : null)
+                .stateId(entity.getState() != null ? entity.getState().getId() : null)
+                .build();
     }
 }
