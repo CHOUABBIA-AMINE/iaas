40ba86fd810029fd9738ee10f231077f36d69a32
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:28:35 +0100

    Fix ClearanceDTO: parameterize GenericDTO<Clearance> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/provider/dto/ClearanceDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/ClearanceDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: ClearanceDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,11 +17,13 @@ package dz.mdn.iaas.business.provider.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.provider.model.Clearance;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Clearance Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ClearanceDTO extends GenericDTO {
+public class ClearanceDTO extends GenericDTO<Clearance> {
 
     private int internalId;
     private String reference;
@@ -46,4 +49,18 @@ public class ClearanceDTO extends GenericDTO {
     private boolean isValid;
     private String observation;
 
+    @Override
+    public Clearance toEntity() {
+        Clearance entity = new Clearance();
+        entity.setId(this.getId());
+        entity.setInternalId(this.internalId);
+        entity.setReference(this.reference);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(Clearance entity) {
+        if (this.internalId > 0) entity.setInternalId(this.internalId);
+        if (this.reference != null) entity.setReference(this.reference);
+    }
 }
