3c0d4d8032bd19d30b93048cac445f8bc7cb3736
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:31:35 +0100

    Fix ProviderExclusionDTO: parameterize GenericDTO<ProviderExclusion> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/provider/dto/ProviderExclusionDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/ProviderExclusionDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: ProviderExclusionDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,11 +17,13 @@ package dz.mdn.iaas.business.provider.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.provider.model.ProviderExclusion;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * ProviderExclusion Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ProviderExclusionDTO extends GenericDTO {
+public class ProviderExclusionDTO extends GenericDTO<ProviderExclusion> {
 
     private int internalId;
     private String reference;
@@ -50,4 +53,18 @@ public class ProviderExclusionDTO extends GenericDTO {
     private boolean isPermanent;
     private String observation;
 
+    @Override
+    public ProviderExclusion toEntity() {
+        ProviderExclusion entity = new ProviderExclusion();
+        entity.setId(this.getId());
+        entity.setInternalId(this.internalId);
+        entity.setReference(this.reference);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(ProviderExclusion entity) {
+        if (this.internalId > 0) entity.setInternalId(this.internalId);
+        if (this.reference != null) entity.setReference(this.reference);
+    }
 }
