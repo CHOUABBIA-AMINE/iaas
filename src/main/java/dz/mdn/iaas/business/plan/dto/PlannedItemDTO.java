78da545102b0186fc9c753b8e2fae8863545ec7e
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:24:08 +0100

    Fix PlannedItemDTO: parameterize GenericDTO<PlannedItem> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/plan/dto/PlannedItemDTO.java
+++ src/main/java/dz/mdn/iaas/business/plan/dto/PlannedItemDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: PlannedItemDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,11 +17,13 @@ package dz.mdn.iaas.business.plan.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.plan.model.PlannedItem;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * PlannedItem Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class PlannedItemDTO extends GenericDTO {
+public class PlannedItemDTO extends GenericDTO<PlannedItem> {
 
     private int internalId;
     
@@ -49,4 +52,18 @@ public class PlannedItemDTO extends GenericDTO {
     private int priority;
     private String observation;
 
+    @Override
+    public PlannedItem toEntity() {
+        PlannedItem entity = new PlannedItem();
+        entity.setId(this.getId());
+        entity.setInternalId(this.internalId);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(PlannedItem entity) {
+        if (this.internalId > 0) {
+            entity.setInternalId(this.internalId);
+        }
+    }
 }
