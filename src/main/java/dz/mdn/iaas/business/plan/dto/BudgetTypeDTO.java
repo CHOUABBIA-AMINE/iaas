f2e704c2c6210b36e5858414c7ee627e8fff7469
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:24:53 +0100

    Fix BudgetTypeDTO: parameterize GenericDTO<BudgetType> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/plan/dto/BudgetTypeDTO.java
+++ src/main/java/dz/mdn/iaas/business/plan/dto/BudgetTypeDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: BudgetTypeDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -16,12 +16,14 @@ package dz.mdn.iaas.business.plan.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.plan.model.BudgetType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * BudgetType Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BudgetTypeDTO extends GenericDTO {
+public class BudgetTypeDTO extends GenericDTO<BudgetType> {
 
     @Size(max = 200, message = "Arabic designation must not exceed 200 characters")
     private String designationAr;
@@ -44,4 +47,26 @@ public class BudgetTypeDTO extends GenericDTO {
     @Size(max = 200, message = "French designation must not exceed 200 characters")
     private String designationFr;
 
+    @Override
+    public BudgetType toEntity() {
+        BudgetType entity = new BudgetType();
+        entity.setId(this.getId());
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(BudgetType entity) {
+        if (this.designationAr != null) {
+            entity.setDesignationAr(this.designationAr);
+        }
+        if (this.designationEn != null) {
+            entity.setDesignationEn(this.designationEn);
+        }
+        if (this.designationFr != null) {
+            entity.setDesignationFr(this.designationFr);
+        }
+    }
 }
