6212cac267319ceb8974551d487ef4d544097f9e
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:25:42 +0100

    Fix FinancialOperationDTO: parameterize GenericDTO<FinancialOperation> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/plan/dto/FinancialOperationDTO.java
+++ src/main/java/dz/mdn/iaas/business/plan/dto/FinancialOperationDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: FinancialOperationDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -17,11 +17,13 @@ package dz.mdn.iaas.business.plan.dto;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.plan.model.FinancialOperation;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * FinancialOperation Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class FinancialOperationDTO extends GenericDTO {
+public class FinancialOperationDTO extends GenericDTO<FinancialOperation> {
 
     private int internalId;
     private String reference;
@@ -58,4 +61,24 @@ public class FinancialOperationDTO extends GenericDTO {
     
     private Long approvalStatusId;
 
+    @Override
+    public FinancialOperation toEntity() {
+        FinancialOperation entity = new FinancialOperation();
+        entity.setId(this.getId());
+        entity.setInternalId(this.internalId);
+        entity.setReference(this.reference);
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(FinancialOperation entity) {
+        if (this.internalId > 0) entity.setInternalId(this.internalId);
+        if (this.reference != null) entity.setReference(this.reference);
+        if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
+        if (this.designationEn != null) entity.setDesignationEn(this.designationEn);
+        if (this.designationFr != null) entity.setDesignationFr(this.designationFr);
+    }
 }
