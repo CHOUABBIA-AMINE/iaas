19f3f67e43c83e46e6bd90b1c0c8102aed2ce026
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-11 09:30:08 +0100

    Fix EconomicDomainDTO: parameterize GenericDTO<EconomicDomain> and implement abstract methods

--- src/main/java/dz/mdn/iaas/business/provider/dto/EconomicDomainDTO.java
+++ src/main/java/dz/mdn/iaas/business/provider/dto/EconomicDomainDTO.java
@@ -4,7 +4,7 @@
  *
  *	@Name		: EconomicDomainDTO
  *	@CreatedOn	: 10-16-2025
- *	@Updated	: 12-10-2025
+ *	@Updated	: 12-11-2025
  *
  *	@Type		: Class
  *	@Layer		: DTO
@@ -16,12 +16,14 @@ package dz.mdn.iaas.business.provider.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dz.mdn.iaas.configuration.template.GenericDTO;
+import dz.mdn.iaas.business.provider.model.EconomicDomain;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * EconomicDomain Data Transfer Object
@@ -29,10 +31,11 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class EconomicDomainDTO extends GenericDTO {
+public class EconomicDomainDTO extends GenericDTO<EconomicDomain> {
 
     @NotBlank(message = "Code is required")
     private String code;
@@ -47,4 +50,22 @@ public class EconomicDomainDTO extends GenericDTO {
     @Size(max = 300, message = "French designation must not exceed 300 characters")
     private String designationFr;
 
+    @Override
+    public EconomicDomain toEntity() {
+        EconomicDomain entity = new EconomicDomain();
+        entity.setId(this.getId());
+        entity.setCode(this.code);
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        return entity;
+    }
+
+    @Override
+    public void updateEntity(EconomicDomain entity) {
+        if (this.code != null) entity.setCode(this.code);
+        if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
+        if (this.designationEn != null) entity.setDesignationEn(this.designationEn);
+        if (this.designationFr != null) entity.setDesignationFr(this.designationFr);
+    }
 }
