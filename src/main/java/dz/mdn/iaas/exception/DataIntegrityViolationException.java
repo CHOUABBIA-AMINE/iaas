f2e76cb0fd19ce68038f3ceadcdbf2add50393a5
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-20 16:55:23 +0100

    feat: Add DataIntegrityViolationException for constraint violations

---
+++ src/main/java/dz/mdn/iaas/exception/DataIntegrityViolationException.java
@@ -0,0 +1,40 @@
+/**
+ *	
+ *	@author		: CHOUABBIA Amine
+ *
+ *	@Name		: DataIntegrityViolationException
+ *	@CreatedOn	: 12-20-2025
+ *
+ *	@Type		: Exception
+ *	@Layer		: Exception
+ *	@Package	: Exception
+ *
+ **/
+
+package dz.mdn.iaas.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Exception thrown when data integrity constraints are violated
+ * Examples: unique constraint, foreign key constraint, not null constraint
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2025-12-20
+ */
+public class DataIntegrityViolationException extends IaasException {
+    
+    public DataIntegrityViolationException(String message) {
+        super("DATA_INTEGRITY_VIOLATION", message, HttpStatus.CONFLICT);
+    }
+    
+    public DataIntegrityViolationException(String constraintName, String details) {
+        super("DATA_INTEGRITY_VIOLATION",
+              String.format("Data integrity violation on '%s': %s", constraintName, details),
+              HttpStatus.CONFLICT);
+    }
+    
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super("DATA_INTEGRITY_VIOLATION", message, HttpStatus.CONFLICT, cause);
+    }
+}
