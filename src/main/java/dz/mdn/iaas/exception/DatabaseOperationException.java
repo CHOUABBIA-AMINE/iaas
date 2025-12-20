0e13616f6e90772f2ce3f0ab3d50b637901d2a42
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-20 16:55:06 +0100

    feat: Add DatabaseOperationException for database operation failures

---
+++ src/main/java/dz/mdn/iaas/exception/DatabaseOperationException.java
@@ -0,0 +1,40 @@
+/**
+ *	
+ *	@author		: CHOUABBIA Amine
+ *
+ *	@Name		: DatabaseOperationException
+ *	@CreatedOn	: 12-20-2025
+ *
+ *	@Type		: Exception
+ *	@Layer		: Exception
+ *	@Package	: Exception
+ *
+ **/
+
+package dz.mdn.iaas.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Exception thrown when database operations fail
+ * Examples: connection failures, query timeouts, transaction failures
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2025-12-20
+ */
+public class DatabaseOperationException extends IaasException {
+    
+    public DatabaseOperationException(String message) {
+        super("DATABASE_ERROR", message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+    public DatabaseOperationException(String message, Throwable cause) {
+        super("DATABASE_ERROR", message, HttpStatus.INTERNAL_SERVER_ERROR, cause);
+    }
+    
+    public DatabaseOperationException(String operation, String details) {
+        super("DATABASE_ERROR", 
+              String.format("Database operation '%s' failed: %s", operation, details),
+              HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
