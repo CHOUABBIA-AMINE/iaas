9a4da1e92e1b81ef11c052003bfd1bf1ddd6064e
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-20 16:55:36 +0100

    feat: Add OptimisticLockException for concurrent modification conflicts

---
+++ src/main/java/dz/mdn/iaas/exception/OptimisticLockException.java
@@ -0,0 +1,37 @@
+/**
+ *	
+ *	@author		: CHOUABBIA Amine
+ *
+ *	@Name		: OptimisticLockException
+ *	@CreatedOn	: 12-20-2025
+ *
+ *	@Type		: Exception
+ *	@Layer		: Exception
+ *	@Package	: Exception
+ *
+ **/
+
+package dz.mdn.iaas.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Exception thrown when optimistic locking fails
+ * Indicates concurrent modification conflict
+ * 
+ * @author CHOUABBIA Amine
+ * @created 2025-12-20
+ */
+public class OptimisticLockException extends IaasException {
+    
+    public OptimisticLockException(String entityName, Object entityId) {
+        super("OPTIMISTIC_LOCK_FAILURE",
+              String.format("The %s with ID %s was modified by another user. Please refresh and try again.",
+                          entityName, entityId),
+              HttpStatus.CONFLICT);
+    }
+    
+    public OptimisticLockException(String message) {
+        super("OPTIMISTIC_LOCK_FAILURE", message, HttpStatus.CONFLICT);
+    }
+}
