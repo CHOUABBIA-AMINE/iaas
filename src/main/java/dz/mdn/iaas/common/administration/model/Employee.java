079c651941f38960f3735aadc9f3e211ffd74d69
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-01 11:22:27 +0100

    Fix: Correct JOINED inheritance mapping - remove conflicting join column annotations
    The error 'An association that targets entity Person has 2 @JoinColumns but the primary key has 1 columns'
    occurs because @PrimaryKeyJoinColumn already defines how Employee joins to Person via F_00.
    
    The @JoinColumn annotations on the ManyToOne relationships (job, structure, militaryRank)
    were being misinterpreted as part of the inheritance join.
    
    Solution: Ensure @JoinColumn references are clean and don't conflict with @PrimaryKeyJoinColumn.

--- src/main/java/dz/mdn/iaas/common/administration/model/Employee.java
+++ src/main/java/dz/mdn/iaas/common/administration/model/Employee.java
@@ -3,7 +3,7 @@
  *	@author		: CHOUABBIA Amine
  *	@Name		: Employee
  *	@CreatedOn	: 06-26-2025
- *	@Updated	: 12-11-2025
+ *	@Updated	: 01-01-2026
  *	@Type		: Model
  *	@Layer		: Common / Administration
  *	@Package	: Common / Administration / Model
@@ -30,9 +30,17 @@ import lombok.ToString;
  * Represents employees with their job, structure, and military information
  * 
  * Uses JOINED inheritance strategy:
- * - Person data is stored in T_01_04_09
- * - Employee-specific data is stored in T_01_04_10
+ * - Person data is stored in T_01_04_09 (parent table)
+ * - Employee-specific data is stored in T_01_04_10 (child table)
  * - Both tables share the same primary key (F_00)
+ * - The join between tables is established via @PrimaryKeyJoinColumn
+ * 
+ * The Employee table (T_01_04_10) has:
+ * - F_00: Primary key (inherited from Person, also acts as FK)
+ * - F_11: Registration Number
+ * - F_12: Job ID (Foreign Key to Job table)
+ * - F_13: Structure ID (Foreign Key to Structure table)
+ * - F_14: Military Rank ID (Foreign Key to Military Rank table)
  */
 @Setter
 @Getter
@@ -42,22 +50,22 @@ import lombok.ToString;
 @AllArgsConstructor
 @Entity(name="Employee")
 @Table(name="T_01_04_10")
-@PrimaryKeyJoinColumn(name="F_00")
+@PrimaryKeyJoinColumn(name="F_00", referencedColumnName="F_00")
 public class Employee extends Person {
 	
 	@Column(name="F_11", length=50)
 	private String registrationNumber;
 	
 	@ManyToOne
-	@JoinColumn(name="F_12")
+	@JoinColumn(name="F_12", referencedColumnName="F_00")
 	private Job job;
 	
 	@ManyToOne
-	@JoinColumn(name="F_13")
+	@JoinColumn(name="F_13", referencedColumnName="F_00")
 	private Structure structure;
 	
 	@ManyToOne
-	@JoinColumn(name="F_14")
+	@JoinColumn(name="F_14", referencedColumnName="F_00")
 	private MilitaryRank militaryRank;
 	
 }
